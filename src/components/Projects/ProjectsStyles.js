cfe0f2f2e6198fb4c77e4ec6018a9ca239f0bcbb
HardikAsher17 <hardik.asher@somaiya.edu>
2021-08-30 18:48:16 +0530

    Made some changes

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Img = styled.img`
   width:400px;
-  height:250px;
+  height:200px;
   object-fit: cover;
   overflow: hidden;
 `
@@ -27,7 +27,7 @@ export const BlogCard = styled.div`
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   width: 400px;
-  height: 750px;
+  height: 675px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
