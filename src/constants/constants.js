68e58a314cbcc910d4242974b59d14abb2dbb72f
HardikAsher17 <hardik.asher@somaiya.edu>
2021-08-31 17:43:44 +0530

    links modified

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,8 +4,8 @@ export const projects = [
     description: "A Fast Mail Service based on sockets(Firebase) with all necessary features like Email, Chat, Video Conference, Spam detection, Encryption, OAuth, etc. ",
       image: '/images/1.png',
       tags: ['React', 'Firebase', 'Machine Learning', 'Cryptography'],
-    source: 'https://github.com/ExpressMailer/ExpressMailer',
-    visit: 'https://express-mailer.netlify.app/',
+    source: 'https://express-mailer.netlify.app/',
+    visit: 'https://github.com/ExpressMailer/ExpressMailer',
     id: 0,
   },
   {
@@ -13,8 +13,8 @@ export const projects = [
     description:"This is a website my friends and I built using HTML and CSS for a start-up called UNLO 3D. This website has all the information about their products, services, contact information and many other things.",
     image: '/images/2.png',
     tags: ['HTML', 'CSS', 'JavaScript'],
-    source: 'https://github.com/HardikAsher17/PrinterWebsite',
-    visit: 'https://unlo-3d.netlify.app/',
+    source: 'https://unlo-3d.netlify.app/',
+    visit: 'https://github.com/HardikAsher17/PrinterWebsite',
     id: 1,
   },
   {
@@ -22,8 +22,8 @@ export const projects = [
     description: "We created a machine learning model using polynomial regression which can be used to predict the covid cases in the near future and will also provide a graphical representation of the actual cases and the predicted cases in a region.",
       image: '/images/3.png',
       tags: ['Machine Learning', 'Artificial Intelligence'],
-    source: 'https://drive.google.com/drive/u/0/search?q=internship',
-    visit: 'https://drive.google.com/drive/u/0/search?q=internship',
+    source: 'https://drive.google.com/file/d/1sxyVPT6UY_qCJle7z2ITBtXBU0Saaudu/view',
+    visit: 'https://drive.google.com/file/d/1sxyVPT6UY_qCJle7z2ITBtXBU0Saaudu/view',
     id: 2,
   },
 ];
