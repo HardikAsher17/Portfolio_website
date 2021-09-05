69976fdfaa2270da1aed62d7312108615564691d
HardikAsher17 <hardik.asher@somaiya.edu>
2021-09-05 13:01:03 +0530

    fixed font 2

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -164,7 +164,7 @@ export const LinkColumn = styled.div`
 export const LinkTitle = styled.h4`
 	font-style: normal;
 	font-weight: 600;
-	font-size: 12px;
+	font-size: 17px;
 	line-height: 24px;
 	text-transform: uppercase;
 	color: rgba(255, 255, 255, 0.4);
